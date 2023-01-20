672c96b654b03dc70778ee9666156b6c8b33d3bf
Nigel <hypej.10@gmail.com>
2023-01-20 11:33:17 +0200

    improve form feedback

--- public/mail.php
+++ public/mail.php
@@ -1,4 +1,21 @@
 <?php
+
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (empty($_POST['phone'])) {
+    $errors['phone'] = 'Phone number is required.';
+}
+
+
 if (isset($_POST['phone'])) {
     $to = "contact@stokdirect.africa"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
@@ -10,9 +27,21 @@ if (isset($_POST['phone'])) {
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+
+    if (!empty($errors)) {
+        $data['success'] = false;
+        $data['errors'] = $errors;
+    } else {
+        $data['success'] = true;
+        mail($to,$subject,$message,$headers);
+        mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+        //echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+        // You can also use header('Location: thank_you.php'); to redirect to another page.
+        $data['message'] = "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    }
+    
+    echo json_encode($data);
+
+    
     }
 ?>
