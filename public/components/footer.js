cc1205c6eb3c592834f879025b575d52fb709992
Nigel <hypej.10@gmail.com>
2023-01-25 17:18:04 +0200

    add more pages

---
+++ public/components/footer.js
@@ -0,0 +1,114 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="bg-[#122B60]" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <img class="h-20" src="../assets/img/stok-logo.svg" alt="Company name">
+        <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-sm font-semibold leading-6 text-white">Product</h3>
+              <ul role="list" class="mt-6 space-y-4">
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Home</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">About Us</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Services</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">How It Works</a>
+                </li>
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Get In Touch</a>
+                </li>
+              </ul>
+            </div>
+            <div class="mt-10 md:mt-0">
+              <h3 class="text-sm font-semibold leading-6 text-white">Use Cases</h3>
+              <ul role="list" class="mt-6 space-y-4">
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Warehouses</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Manufactures</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Retailers</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Consumers</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-sm font-semibold leading-6 text-white">Resources</h3>
+              <ul role="list" class="mt-6 space-y-4">
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Terms and Conditions</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Refund Policy</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Buyer Protection</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Registration</a>
+                </li>
+
+
+              </ul>
+            </div>
+            <div class="mt-10 md:mt-0">
+              <h3 class="text-sm font-semibold leading-6 text-white">Company</h3>
+              <ul role="list" class="mt-6 space-y-4">
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 ">About Us</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Careers</a>
+                </li>
+
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">FAQs</a>
+                </li>
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Teams</a>
+                </li>
+                <li>
+                  <a href="#" class="text-sm leading-6 text-gray-300 hover:text-white">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+    `;
+  }
+}
+
+customElements.define("footer-component", Footer);
---
+++ public/components/nav.js
@@ -0,0 +1,77 @@
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+  <nav class="bg-[#3768b100] z-40 absolute left-0 right-0 top-0">
+  <div class="w-full px-2 sm:px-4 lg:px-8">
+    <div class="flex h-16 justify-between">
+      <div class="flex items-center px-2 lg:px-0">
+        <button type="button" onclick="openNav()"
+          class="inline-flex dropbtn lg:hidden items-center justify-center rounded-md p-2 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white mr-3 -ml-3"
+          aria-controls="myDropdown" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+
+          <img src="../assets/img/menu-white.png" alt="" />
+        </button>
+
+        <div class="hidden lg:ml-4 lg:flex lg:items-center">
+          <img src="../assets/img/stok-logo.svg" class="h-16" alt="" />
+        </div>
+        <!-- Sidebar Navigation -->
+        <div id="mySidenav" class="sidenav z-40 flex flex-col">
+          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+          <a href="#" class="flex items-center">
+
+            <span>Home</span>
+          </a>
+          <a href="#" class="flex items-center">
+
+            <span>About Us</span>
+          </a>
+          <a href="#" class="flex items-center">
+
+            <span>Services</span>
+          </a>
+          <a href="#" class="flex items-center">
+
+            <span>FAQs</span>
+          </a>
+          <a href="#" class="flex items-center">
+
+            <span>Contact</span>
+          </a>
+          <a href="#" class="flex items-center">
+
+            <span>Register</span>
+          </a>
+
+
+        </div>
+      </div>
+      <div class="lg:flex flex-1 hidden items-center justify-center px-2 lg:ml-6 lg:justify-end">
+        <div class="max-w-lg lg:max-w-md flex items-center justify-evenly w-full">
+          <a class="text-white hover:text-gray-200" href="#home">Home</a>
+          <a class="text-white hover:text-gray-200" href="#about">About Us</a>
+          <a class="text-white hover:text-gray-200" href="#services">Services</a>
+          <a class="text-white hover:text-gray-200" href="#faq">FAQs</a>
+          <a class="text-white hover:text-gray-200" href="#contact">Contact</a>
+        </div>
+      </div>
+
+      <div class="hidden lg:ml-4 lg:flex lg:items-center">
+        <a class="bg-[#E2BF17] rounded-md text-bold px-4 py-2" href="#">Register</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+</nav>
+
+  `;
+  }
+}
+
+customElements.define("nav-component", NavBar);
